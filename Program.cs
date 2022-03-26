e3eb8908ff370d6c3f9c7f29b165b678f0cf105e
leeool <lgonsalez02@gmail.com>
2022-03-25 22:11:39 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double metros;
+
+Console.Write("Entre com metros: ");
+metros = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Km = {metros / 1000}");
+Console.WriteLine($"Cm = {metros * 100}");
+Console.WriteLine($"M = {metros}");
